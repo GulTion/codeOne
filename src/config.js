ad0eb70f761c717ce48511aa56bf82703dee07d3
GulTion <44129143+GulTion@users.noreply.github.com>
2024-05-09 13:32:23 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // export const RUN_SERVER = "http://127.0.0.1:3001/run";
 // export const SERVER = "http://127.0.0.1:3001";
-export const RUN_SERVER = "https://c0d3Server.gultion.repl.co/run";
-export const SERVER = "https://c0d3Server.gultion.repl.co";
+export const RUN_SERVER = "https://09f0808c-f0e7-4eda-9824-c3565048f8de-00-1c710ypdqx5wz.spock.replit.dev/run";
+export const SERVER = "https://09f0808c-f0e7-4eda-9824-c3565048f8de-00-1c710ypdqx5wz.spock.replit.dev";
